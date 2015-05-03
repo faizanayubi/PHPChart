15bccc36db01c8dcc9fdac54b5845d27702e8449
faizanayubi <indianayubi@gmail.com>
2015-05-03 21:41:16 +0530

    Created Chart Class

---
+++ Chart.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Main Class to Draw Chart and Graphs
+ *
+ * @author Faizan Ayubi
+ */
+class Chart {
+    public $data;
+    
+    public function __construct($data) {
+        $this->data = $data;
+    }
+}
