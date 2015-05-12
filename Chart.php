f3c5aa7bcd94c22ffa6697b006973297f772fcbd
MERAJ AHMAD SIDDIQUI <merajsiddiqui@outlook.com>
2015-05-12 20:35:31 +0100

    PIE graph

--- Chart.php
+++ Chart.php
@@ -71,6 +71,37 @@ class Chart {
         }
         $this->img = $img;
     }
+    function drawPie($img_width = 500, $img_height = 500, $margins = 0){
+        //set total sum = 0
+        $data_sum = array_sum($this->data);
+        //finding no of sectors
+        $total_sectors = count($this->data);
+        $angle_sum = array(-1 => 0, 360);
+        $img = imagecreatetruecolor($img_width, $img_height);
+        //defining variable colours for image
+       $color = array( array(50, 30, 90), array(120, 240, 30),
+        array(180,150,130), array(201, 240, 189), array(154, 198, 86),
+        array(89, 163, 199), array(108, 213, 97), array(69, 250, 230),
+        array(169, 250, 130), array(95, 150, 20), array(209, 29, 70), array(102, 201, 8));
+       shuffle($color);
+       $colors = array(imagecolorallocate($img, $color[0][0], $color[0][1], $color[0][2]));
+       $colord = array(imagecolorallocate($img, ($color[0][0] / 1.5), ($color[0][1] / 1.5), ($color[0][2] / 1.5)));
+            for($i=0; $i<$total_sectors; $i++){
+            $angle[$i] = (($this->data[key($this->data)] / $data_sum) * 360);
+            $angle_sum[$i+1] = array_sum($angle);
+            $colors[$i] = imagecolorallocate($img, $color[$i][0], $color[$i][1], $color[$i][2]);
+            $bg_color[$i] = imagecolorallocate($img, ($color[$i][0] / 1.5), ($color[$i][1] / 1.5), ($color[$i][2] / 1.5));
+            $txtcolor[$i] =imagecolorallocate($img, ($color[$i][0] / 2.5), ($color[$i][1] /2.5), ($color[$i][2] / 2.5));
+        }
+        //end of for loop\
+        //length of chord = 2rsin(c/2)
+        for($i=0; $i<$total_sectors; $i++){
+            imagefilledarc($img, $img_width/2, $img_height/2, $img_width, $img_height, $angle_sum[$i-1], $angle_sum[$i], $bg_color[$i], IMG_ARC_PIE);
+        }
+        //end of sectors
+
+       return $this->img= $img;
+    }
 
     /**
      * Adjust Margin for graphs
